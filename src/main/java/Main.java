4ef0b3b68b09abcb2779a00d4b5355cb0e030f97
Anug Saha <anugsaha@gmail.com>
2022-06-28 14:01:08 -0400

    use upperbounds to calculate average

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,23 @@
+import java.util.List;
+
 public class Main {
     // implement the public averageList method here
-    
+    public static Double averageList(List<? extends Number> numberList) {
+        double sum = 0.0;
+
+        for (Number number : numberList) {
+            sum += number.doubleValue();
+        }
+
+        double value = sum / (numberList.size()); 
+        return value; 
+    }
+
     public static void main(String[] args) {
         List<Integer> intList = List.of(1, 2, 3, 4, 5);
         System.out.println(averageList(intList)); // 3.0
 
         List<Double> doubleList = List.of(1.0, 2.0, 3.0, 4.0, 5.0);
-        System.out.println(averageList(doubleList)); // 3.0       
+        System.out.println(averageList(doubleList)); // 3.0
     }
 }
